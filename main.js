2c6a5767a86d61ded32b498eb08e881f6d998974
langat justus <justuslangat78@gmail.com>
2020-09-23 19:24:06 +0300

    write a function that counts the minutes and seconds

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 var minutes = 25;
 var seconds = "00";
 
+var click = new Audio("click.mp3");
+var bell = new Audio ("bell.mp3");
+
 function template(){
     document.getElementById("minutes").innerHTML= minutes;
     document.getElementById("seconds").innerHTML= seconds;
 }
+
+function start(){
+    click.play();
+
+    minutes = 24;
+    seconds = 59;
+
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+}
+
